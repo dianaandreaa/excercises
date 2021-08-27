dee9c5de5ef6f4864c13ee7a21bd4878de77c58c
dianaandreaa <78021758+dianaandreaa@users.noreply.github.com>
2021-08-27 09:48:17 +0200

    excercises

---
+++ use_strict.js
@@ -0,0 +1,13 @@
+"use strict";
+let y = 14;
+
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+    cats: 2,
+    cars: 1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
