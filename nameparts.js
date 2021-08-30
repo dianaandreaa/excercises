a099b232cce9bc3e8ecfdb8901928313cc400342
dianaandreaa <78021758+dianaandreaa@users.noreply.github.com>
2021-08-30 15:47:04 +0200

    name parts

---
+++ nameparts.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const str =  "Albus Percival Wulfric Brian Dumbledore";
+
+console.log(str.substring(str.indexOf(" ")+1, str.lastIndexOf(" ")));
+
+console.log(str.split(" ")[1]);
--- use_strict.js
+++ use_strict.js
@@ -11,3 +11,22 @@ const obj = {
 const nothing = null;
 let undf;
 const symbol = Symbol("symbol");
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "a";
+const s2 = "2";
+
+n1 + n2;
+s1 + s2;
+
+n1 + s2;
+s1 + n2;
+
+const value = "";
+if (value){
+    console.log("Value is truthy");
+} else {
+    console.log("Value is falsey")
+}
