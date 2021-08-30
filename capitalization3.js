7a4f4cf525ce401e3c7be07e32cd0228e9105fbc
dianaandreaa <78021758+dianaandreaa@users.noreply.github.com>
2021-08-30 16:33:36 +0200

    excercise

--- capitalization3.js
+++ capitalization3.js
@@ -1,2 +1,9 @@
 "use strict";
 
+let names =  "Peter";
+
+let newName = names[0].toLowerCase()+ names.substring(1);
+
+console.log(names.toUpperCase());
+console.log(newName);
+console.log(names.substring(0, 3).toLowerCase()+names.substring(3).toUpperCase());
